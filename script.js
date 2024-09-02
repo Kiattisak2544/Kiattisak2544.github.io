968912f1acba93f2a8f56e6066463341b2069d44
kiattisak2541 <kiattisakjampradang@gmail.com>
2024-09-02 16:27:50 +0700

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-$(document).ready(function(){
-  var navbar = $('.navbar');
-  var currentUrl = window.location.href;
-
-  $(window).scroll(function(){
-      if($(window).scrollTop() <= 40){
-         navbar.removeClass("fixed-top ");
-      }else{
-         navbar.addClass("fixed-top");
-      }
-  });
-  
-});
+$(document).ready(function(){var navbar=$('.navbar');var currentUrl=window.location.href;$(window).scroll(function(){if($(window).scrollTop()<=40){navbar.removeClass("fixed-top ")}else{navbar.addClass("fixed-top")}})})
